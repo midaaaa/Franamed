e60f3832c6e6f60565ae43c9ec2bd21defd75c94
midaaaa <fildima8423@gmail.com>
2026-08-13 15:27:55 +0300

    Initial commit

---
+++ Franamed/FranamedApp.swift
@@ -0,0 +1,17 @@
+//
+//  FranamedApp.swift
+//  Franamed
+//
+//  Created by Дмитрий Филимонов on 11.08.2026.
+//
+
+import SwiftUI
+
+@main
+struct FranamedApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
